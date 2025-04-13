8932a4888ac853deb51b487e1890262bcad5bc66
saclark1978 <stephanie.clark@uwgkc.org>
2025-04-13 00:12:00 +0000

    FinalCommit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,50 @@
 const addDevSkill = function (skill) {
-    let listItem = document.createElement('li');
-    listItem.innerHTML = skill;
-    document.getElementById("coding-section").appendChild(listItem);
+	let listItem = document.createElement('li');
+	listItem.innerHTML = skill;
+	document.getElementById("coding-section").appendChild(listItem);
 };
 
-const drink = function (item) {
-   return 'I\'d like a ' + item + ', please!';
+
+
+const request = function (item) {
+	return 'I\'d like a ' + item + ', please!';
 };
- 
-const Stephanie = 'cocktail';
+const drink = 'cocktail';
+
 
 let numberOfClicks = 0;
-
 const clickHandler = function (text) {
-    numberOfClicks = numberOfClicks + 1;
-    console.log('numberOfClicks', numberOfClicks);
-    if (numberOfClicks <= 3) {
-        alert(text);
-    } else {
-        alert("Drink in moderation-- no more cocktails for you!");
-    }
-}
+	numberOfClicks = numberOfClicks + 1;
+	console.log('numberOfClicks', numberOfClicks);
+
+	if (numberOfClicks <= 3) {
+		alert(text);
+	} else {
+		alert("Drink in moderation-- no more cocktails for you!")
+	}
+};
+
+
 // Declare brainSkills array below this line
+const brainSkills = [
+	"Front-End Architecture",
+	"HTML",
+	"CSS",
+	"Command line operations",
+	"Vim",
+	"Git",
+	"JavaScript Types"
+   ];
+
+
 
 document.addEventListener("DOMContentLoaded", function (event) {
 
-   document.getElementById('cocktail-section').innerHTML = drink(Stephanie);
+
+	document.getElementById('cocktail-section').innerHTML = request(drink);
+
+	brainSkills.forEach(function (element) {
+		addDevSkill(element);
+	});
 
 });
